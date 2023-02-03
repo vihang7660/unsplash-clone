5fc4e9910a2443d96e6e9a5e6aeddbcea54457cd
Vihang Supare <vihang7660@gmail.com>
2023-02-03 12:41:31 +0530

    Added home image with text on it

---
+++ src/components/Wall.tsx
@@ -0,0 +1,15 @@
+import wall from "../assets/wall.jpg";
+
+export default function Wall() {
+  return (
+    <div className="pt-32">
+      <div className="background-image">
+        <div className=" pt-36 pl-48">
+          <h1 className="header">Unsplash</h1>
+          <p className="subheader">The internet’s source for visuals.</p>
+          <p className="subheader">Powered by creators everywhere.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
