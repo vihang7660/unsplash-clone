5adaddee80187295be450a191aaadfcd2efe2b98
Vihang Supare <vihang7660@gmail.com>
2023-02-03 15:57:54 +0530

    not to render stock wall photo when searching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { useSelector } from "react-redux";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Wall from "./components/Wall";
 import PhotosList from "./features/photos/PhotosList";
 
+interface State {
+  users: any;
+}
+
 const App: React.FC = () => {
+  const { query } = useSelector((state: State) => state.users);
+  function renderWall() {
+    if (!query) {
+      return <Wall />;
+    }
+  }
+
   return (
     <div className="App">
       <Header />
       <Navbar />
-      <Wall />
+      {renderWall()}
       <PhotosList />
     </div>
   );
